7d8acf702a6e7394526e05cdee087799025ff86c
Laurice Howard <lowkeylaurice@gmail.com>
2021-09-15 23:21:23 -0400

    code lying around

--- src/components/Open-Weather.tsx
+++ src/components/Open-Weather.tsx
@@ -3,10 +3,7 @@ import Geolocation from './Geolocation'
 
 
 const OpenWeather = () => {
-    //b7bd8014245284ce125d6de5bde9b301
-    /* http://api.openweathermap.org/data/2.5/weather? */
     return (<>
-    {/* called class component */}
     <Geolocation/>
     </>)
 }
